231643b76f999fea4aeaef043b8129f46107492f
Hendri Pretorius <pretorh@gmail.com>
2013-11-03 16:39:10 +0200

    get episode list, get series details

--- src/tvrage.js
+++ src/tvrage.js
@@ -2,11 +2,21 @@ module.exports = {
     search: function(series, getXmlFromUrl, callback) {
         var values = search.queryObject(series);
         makeApiCall(getXmlFromUrl, search, values, callback);
+    },
+    getEpisodeList: function(seriesId, getXmlFromUrl, callback) {
+        var values = eplist.queryObject(seriesId);
+        makeApiCall(getXmlFromUrl, eplist, values, callback);
+    },
+    getSeriesDetails: function(seriesId, getXmlFromUrl, callback) {
+        var values = eplist.queryObject(seriesId);
+        makeApiCall(getXmlFromUrl, seriesinfo, values, callback);
     }
 };
 
 var querystring = require("querystring"),
-    search = require("./search");
+    search = require("./search"),
+    eplist = require("./eplist"),
+    seriesinfo = require("./seriesinfo");
 
 const API_ROOT_URL = "http://services.tvrage.com/feeds/";
 
--- test/http-test.js
+++ test/http-test.js
@@ -43,5 +43,73 @@ vows.describe("http test").addBatch({
         "the first entry of the result is *Modern Family*": function(err, res) {
             values.isModernFamily(res.result[0]);
         }
+    },
+    
+    "when listing a series' episodes": {
+        topic: function(mock) {
+            var cb = this.callback;
+            var mock = new GetXmlMock("episode_list.xml");
+            tvrage.getEpisodeList(22622, mock.get, function(err, result) {
+                cb(err, {
+                    mock: mock,
+                    result: result
+                });
+            });
+        },
+        "the get xml over http function got called once with url=*...episode_list.php...*": function(err, res) {
+            assert.equal(res.mock.calls.length, 1);
+            assert.equal(res.mock.calls[0], "http://services.tvrage.com/feeds/episode_list.php?sid=22622");
+        },
+        "no error occured": function(err, res) {
+            assert.isNull(err);
+        },
+        "the result is an array": function(err, res) {
+            assert.isNotNull(res);
+            assert.isNotNull(res.result);
+            assert.isArray(res.result);
+        },
+        "the *first episode* of the *first season* is *Pilot*": function(err, res) {
+            assert.isNotNull(res.result[0]);
+            assert.isNotNull(res.result[0].list);
+            assert.isNotNull(res.result[0].list[0]);
+            values.isPilot(res.result[0].list[0]);
+        },
+        "the *second episode* of the *second season* is *The Kill*": function(err, res) {
+            assert.isNotNull(res.result[1]);
+            assert.isNotNull(res.result[1].list);
+            assert.isNotNull(res.result[1].list[1]);
+            values.isTheKiss(res.result[1].list[1]);
+        }
+    },
+    
+    "when getting a series' details": {
+        topic: function(mock) {
+            var cb = this.callback;
+            var mock = new GetXmlMock("episodeinfo.xml");
+            tvrage.getSeriesDetails(22622, mock.get, function(err, result) {
+                cb(err, {
+                    mock: mock,
+                    result: result
+                });
+            });
+        },
+        "the get xml over http function got called once with url=*...episodeinfo.php...*": function(err, res) {
+            assert.equal(res.mock.calls.length, 1);
+            assert.equal(res.mock.calls[0], "http://services.tvrage.com/feeds/episodeinfo.php?sid=22622");
+        },
+        "no error occured": function(err, res) {
+            assert.isNull(err);
+        },
+        "the result is an object": function(err, res) {
+            assert.isNotNull(res);
+            assert.isNotNull(res.result);
+            assert.isObject(res.result);
+        },
+        "the result has details for *Modern Family*": function(err, res) {
+            values.hasModernFamilyDetails(res.result);
+            values.hasModernFamilyAirtime(res.result);
+            values.hasModernFamilyLatestEp(res.result);
+            values.hasModernFamilyNextEp(res.result);
+        }
     }
 }).export(module);
